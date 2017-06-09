1a9fb6c880388f467c052a9a1f1dfaeb02c094ff
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-06-09 16:04:58 -0700

    fixup! BIP0144+wire: add a MessageEncoding variant for serialization/deserialization

--- wire/msgblock_test.go
+++ wire/msgblock_test.go
@@ -325,8 +325,6 @@ func TestBlockSerialize(t *testing.T) {
 	}
 }
 
-// TODO(roasbeef): tests for SerializeSizeWitness
-
 // TestBlockSerializeErrors performs negative tests against wire encode and
 // decode of MsgBlock to confirm error paths work correctly.
 func TestBlockSerializeErrors(t *testing.T) {
